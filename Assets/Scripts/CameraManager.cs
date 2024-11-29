8dfcf99edee323c8a3d1f1288860bc355a8106de
nhglud <nhglud@gmail.com>
2024-11-29 11:46:52 +0100

    Camera refactor
    removed ''magic numbers''

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,29 +5,34 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private Transform player1Transform;
     [SerializeField] private Transform player2Transform;
     [SerializeField] private LightSource lightSource;
+
     private Light light;
-    private Vector3 playerMidPoint;
+    private Vector3 playersMidPoint;
     private float distanceBetweenPlayers;
+    private float cameraYOffset = 8;
+    private float cameraZOffset = 8;
+    private float lerpModifier = 1.5f;
+    private float distanceModifier = 1f;
 
 
     void Start()
     {
         light = lightSource.GetComponentInChildren<Light>();
-        playerMidPoint = (player1Transform.position + player2Transform.position) / 2;
+        playersMidPoint = (player1Transform.position + player2Transform.position) / 2;
 
     }
 
-   
+
     void Update()
     {
-        playerMidPoint = (player1Transform.position + player2Transform.position) / 2;
+        playersMidPoint = (player1Transform.position + player2Transform.position) / 2;
 
         distanceBetweenPlayers = Vector3.Distance(player1Transform.position, player2Transform.position);
 
         transform.position = Vector3.Lerp(transform.position,
-                                                     new Vector3(playerMidPoint.x, 
-                                                                 0.5f * distanceBetweenPlayers + 8,
-                                                                 playerMidPoint.z - 5),
-                                                     Time.deltaTime);
+                                                     new Vector3(playersMidPoint.x,
+                                                                 distanceModifier * distanceBetweenPlayers + cameraYOffset,
+                                                                 playersMidPoint.z - cameraZOffset),
+                                                     Time.deltaTime * lerpModifier);
     }
 }
