0e19e1d5ff7676c65f5ec860dc75279053240bb4
nhglud <nhglud@gmail.com>
2024-11-29 11:31:00 +0100

    CameraManager Refectoring

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,24 +4,30 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Transform player1Transform;
     [SerializeField] private Transform player2Transform;
-    [SerializeField] private Camera mainCamera;
     [SerializeField] private LightSource lightSource;
     private Light light;
+    private Vector3 playerMidPoint;
+    private float distanceBetweenPlayers;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         light = lightSource.GetComponentInChildren<Light>();
-        transform.position = (player1Transform.position + player2Transform.position) / 2;
+        playerMidPoint = (player1Transform.position + player2Transform.position) / 2;
+
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        transform.position = (player1Transform.position + player2Transform.position) / 2;
-        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position,
-                                                     new Vector3(transform.position.x, 1.5f * light.intensity, transform.position.z - 0.9f *light.intensity),
+        playerMidPoint = (player1Transform.position + player2Transform.position) / 2;
+
+        distanceBetweenPlayers = Vector3.Distance(player1Transform.position, player2Transform.position);
+
+        transform.position = Vector3.Lerp(transform.position,
+                                                     new Vector3(playerMidPoint.x, 
+                                                                 0.5f * distanceBetweenPlayers + 8,
+                                                                 playerMidPoint.z - 5),
                                                      Time.deltaTime);
     }
 }
