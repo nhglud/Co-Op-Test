e1ee032172557527f4a7835908c3b3a4a656808c
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2024-12-02 13:50:36 +0100

    Auto aim
    The script GunAim has been made and is used to aim at closed enemy

--- Assets/Scripts/GunAuto.cs
+++ Assets/Scripts/GunAuto.cs
@@ -10,7 +10,9 @@ public class GunAuto : MonoBehaviour
     public float bulletspeed = 20; // speed of the bullet
 
     private float nextbullet; // tracking for the next shoot
-    
+
+    [SerializeField] GunAim gunAim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +33,11 @@ public class GunAuto : MonoBehaviour
     {
         //Instatiate a bullet at the bulletspawn
         GameObject bulletshot = Instantiate(bullet, bulletspawn.transform);
+
+        Vector3 aimDir = gunAim.updateAimDirection();
+
+        
+        bulletspawn.transform.forward = gunAim.updateAimDirection();
 
         //Add velocity
         bulletshot.GetComponent<Rigidbody>().velocity = bulletspawn.transform.forward.normalized * bulletspeed;
