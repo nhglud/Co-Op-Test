7899617a6d75e650f8787ec13856f8140d792234
sofi222g <sofiezhuksorensen@gmail.com>
2024-12-06 11:10:17 +0100

    ff

--- Assets/Scripts/GunAuto.cs
+++ Assets/Scripts/GunAuto.cs
@@ -72,7 +72,7 @@ public class GunAuto : MonoBehaviour
     {
         fireRate = 0.5f;
         bulletspeed = 20; // speed of the bullet
-        weight = 4;
+        weight = 8;
         bulletdamage = 10;
     }
 
@@ -80,7 +80,7 @@ public class GunAuto : MonoBehaviour
     {
         fireRate = 2;
         bulletspeed = 20; // speed of the bullet
-        weight = 2;
+        weight = 6;
         bulletdamage = 1;
     }
 
@@ -88,7 +88,7 @@ public class GunAuto : MonoBehaviour
     {
         fireRate = 6;
         bulletspeed = 20; // speed of the bullet
-        weight = 6;
+        weight = 10;
         bulletdamage = 5;
     }
 
